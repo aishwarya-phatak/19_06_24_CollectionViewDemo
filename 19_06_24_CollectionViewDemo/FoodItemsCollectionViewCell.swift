c11934e6363bb1561e8e6d3b9ccff12dfb0dcf21
aishwarya <ashp17star@gmail.com>
2024-07-26 13:05:15 +0530

    Collection View Controller with cell deisgn and backgraong view, custom cell

---
+++ 19_06_24_CollectionViewDemo/FoodItemsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FoodItemsCollectionViewCell.swift
+//  19_06_24_CollectionViewDemo
+//
+//  Created by Vishal Jagtap on 26/07/24.
+//
+
+import UIKit
+
+class FoodItemsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
